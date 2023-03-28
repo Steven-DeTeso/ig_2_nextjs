9110757153fd9f843351db76c8cdf97ba8dff333
Steven DeTeso <sadeteso@gmail.com>
2023-03-28 19:03:48 -0400

    homepage and posts code transfered over

--- app/home/page.js
+++ app/home/page.js
@@ -3,7 +3,7 @@
 import React, { useState, useEffect } from "react";
 import Post from "../post/page";
 
-export default function Home() {
+export default function HomePage() {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
@@ -11,10 +11,11 @@ export default function Home() {
       try {
         const response = await fetch("http://localhost:8000/posts/", {
           headers: {
-            Authorization: `Bearer 'token'`,
+            Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjgwMDQ3MDE5LCJpYXQiOjE2ODAwNDM0MTksImp0aSI6IjEyY2E0NzIxMTg2YTQ3MGY5NzhiYTBjMjFkYTVkOGZiIiwidXNlcl9pZCI6MTgsImVtYWlsIjoiVGVzdHRlc3RAZ21haWwuY29tIn0.8qdrXLcW74J9887ynxpyip3miqD5N6xZJd51KSWseK0`,
           },
         });
         const data = await response.json();
+        console.log(data);
         setPosts(data);
       } catch (error) {
         console.log("Error fetching posts:", error);
--- app/post/page.js
+++ app/post/page.js
@@ -1,14 +1,14 @@
 import React from "react";
 
 export default function Post({ post }) {
-  if (!post.image || !post.image[0] || !post.image[0].signed_image_url) {
+  if (!post.images || !post.images[0] || !post.images[0].signed_image_url) {
     return null;
   }
   return (
     <div className="post-container">
       <h3>{post.author.username}</h3>
       <img
-        src={post.image[0].signed_image_url}
+        src={post.images[0].signed_image_url}
         alt={post.caption}
         className="post-image"
       />
